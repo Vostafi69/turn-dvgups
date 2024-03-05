af3991395c1dc2a263d2382bba588b40e710ad05
Alexander <thorik.sanya@gmail.com>
2024-03-06 00:13:06 +1000

    Короткая ссылка для доступа к изображениям и скриптам

--- src/middlewares/app.middleware.js
+++ src/middlewares/app.middleware.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const homeRouter = require("../routes/home.router");
+const { NODE_ENV } = require("../utils/constants");
 
-app.use("/public", express.static(path.join(__dirname, "../../dev")));
+const public = NODE_ENV === "development" ? "dev" : "build";
 
+app.set("view engine", "ejs");
+app.use("/public", express.static(path.join(__dirname, `../../${public}`)));
+app.use("/img", express.static(path.join(__dirname, `../../${public}/img`)));
+app.use("/js", express.static(path.join(__dirname, `../../${public}/js`)));
 app.use("/", homeRouter);
-
-app.use((_, res) => {
+app.use((_req, res) => {
   res.status(404).send("Not Found");
 });
 
