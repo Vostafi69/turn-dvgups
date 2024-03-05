aa7c5ae038eec6f4f4a98349b845175097b7015f
Alexander <thorik.sanya@gmail.com>
2024-03-06 00:09:23 +1000

    Добавлена новая константа NODE_ENV для деплоя и разработки

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 const IP = process.env.IP;
+const NODE_ENV = process.env.NODE_ENV;
 
-module.exports = { PORT, IP };
+module.exports = { PORT, IP, NODE_ENV };
