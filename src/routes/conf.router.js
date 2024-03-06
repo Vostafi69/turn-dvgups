ab453361cff48d8540d55aa0508a14dd819745ef
Alexander <thorik.sanya@gmail.com>
2024-03-07 01:25:08 +1000

    Переименован роут homerout в confrout, изменены пути

---
+++ src/routes/conf.router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const confRouter = express.Router();
+const confController = require("../controllers/conf.controller");
+
+confRouter.get("/", confController.index);
+
+confRouter.get("/create-conf", confController.createConf);
+
+confRouter.get("/join-conf", confController.joinConf);
+
+module.exports = confRouter;
--- src/routes/home.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const homeRouter = express.Router();
-const homeController = require("../controllers/home.controller");
-
-homeRouter.get("/", homeController.index);
-
-homeRouter.get("/create-conf", homeController.createConf);
-
-module.exports = homeRouter;
