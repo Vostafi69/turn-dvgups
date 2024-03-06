8d910c31936a5d73e2530a3f5f7bdf6d42337df9
Alexander <thorik.sanya@gmail.com>
2024-03-07 01:14:50 +1000

    Подключена динамическая генерация breadcrumbs, изменена стартовая страница

--- src/middlewares/app.middleware.js
+++ src/middlewares/app.middleware.js
@@ -1,16 +1,38 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const path = require("path");
-const homeRouter = require("../routes/home.router");
+const confRouter = require("../routes/conf.router");
+const createBreadcrumbs = require("../helpers/createBreadcrumps.helper");
 const { NODE_ENV } = require("../utils/constants");
 
 const public = NODE_ENV === "development" ? "dev" : "build";
 
+// engine
 app.set("view engine", "ejs");
-app.use("/public", express.static(path.join(__dirname, `../../${public}`)));
-app.use("/img", express.static(path.join(__dirname, `../../${public}/img`)));
-app.use("/js", express.static(path.join(__dirname, `../../${public}/js`)));
-app.use("/", homeRouter);
+app.use(expressLayouts);
+
+// static
+app.set("layout", path.join(__dirname, "../views/layouts/layout.ejs"));
+app.use("*/public", express.static(path.join(__dirname, `../../${public}`)));
+app.use("*/img", express.static(path.join(__dirname, `../../${public}/img`)));
+app.use("*/js", express.static(path.join(__dirname, `../../${public}/js`)));
+app.use("*/libs", express.static(path.join(__dirname, `../../${public}/libs`)));
+
+// breadcrumbs
+app.use(function (req, _res, next) {
+  req.breadcrumbs = createBreadcrumbs(req.originalUrl);
+  next();
+});
+
+// home
+app.get("/", (_req, res) => {
+  res.redirect("conf");
+});
+
+// routes
+app.use("/conf", confRouter);
+
 app.use((_req, res) => {
   res.status(404).send("Not Found");
 });
