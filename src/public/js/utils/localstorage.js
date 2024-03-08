0e8e1d9bef5a5ea103d31d7b71f92d15950f196a
Alexander <thorik.sanya@gmail.com>
2024-03-08 17:02:36 +1000

    Написан класс обрертка для работы с localstorage

---
+++ src/public/js/utils/localstorage.js
@@ -0,0 +1,34 @@
+class LocalStorage {
+  constructor(key) {
+    this._key = key;
+  }
+
+  getItem() {
+    const storage = localStorage.getItem(this._key);
+
+    let _storage = false;
+
+    if (storage) {
+      try {
+        _storage = JSON.parse(storage);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    return _storage;
+  }
+
+  setItem(value) {
+    const _value = JSON.stringify(value);
+    localStorage.setItem(this._key, _value);
+
+    return true;
+  }
+
+  removeItem() {
+    localStorage.removeItem(this._key);
+  }
+}
+
+module.exports = LocalStorage;
