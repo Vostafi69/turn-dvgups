c5dbcc989ba3e07426fe1bd4181ccbab482338c6
Alexander Thorik <a.thorik@yandex.ru>
2024-05-05 00:34:37 +1000

    Add: добавлена страница для просмотра открытых конференций

--- src/controllers/conf.controller.js
+++ src/controllers/conf.controller.js
@@ -34,3 +34,10 @@ exports.room = (req, res) => {
     layout: path.join(__dirname, "../views/layouts/room-layout.ejs"),
   });
 };
+
+exports.publicRooms = (req, res) => {
+  res.render(createPath("public-rooms"), {
+    props: { title: "Открытые конференции", breadcrumbs: req.breadcrumbs },
+    layout: path.join(__dirname, "../views/layouts/public-rooms-layout.ejs"),
+  });
+};
