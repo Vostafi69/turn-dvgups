c125e5810589f3bf897dd6d3aff69960d4b08bb8
Alexander Thorik <a.thorik@yandex.ru>
2024-05-05 00:37:19 +1000

    Update: добавлен новй путь для хлебных крошек

--- src/utils/breadcrumbs.js
+++ src/utils/breadcrumbs.js
@@ -3,6 +3,7 @@ const breadcrumbs = {
   "JOIN-CONF": "Подключение к конференции",
   "CREATE-CONF": "Создание конференции",
   CONF: "Конференции",
+  "PUBLIC-ROOMS": "Открытые конференции",
 };
 
 module.exports = breadcrumbs;
