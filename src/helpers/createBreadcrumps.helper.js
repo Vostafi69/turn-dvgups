4c43b1758b194e17ba61cea09b236b6d16fd14c7
Alexander Thorik <a.thorik@yandex.ru>
2024-05-14 14:41:54 +1000

    update: мелкие правки

--- src/helpers/createBreadcrumps.helper.js
+++ src/helpers/createBreadcrumps.helper.js
@@ -3,12 +3,12 @@ const { DOMAIN } = require("../utils/constants");
 
 createBreadcrumbs = function (url) {
   let rtn = [{ name: breadcrumbs["HOME"], url: `https://${DOMAIN}/` }],
-    acc = "";
+    acc = `https://${DOMAIN}/`;
   const urls = url.substring(1).split("/");
 
   for (i = 0; i < urls.length; i++) {
     if (i != urls.length - 1) {
-      acc = acc + "/" + urls[i];
+      acc = acc + urls[i] + "/";
     }
     const translated = breadcrumbs[urls[i].toUpperCase()];
     rtn[i + 1] = { name: translated, url: acc };
